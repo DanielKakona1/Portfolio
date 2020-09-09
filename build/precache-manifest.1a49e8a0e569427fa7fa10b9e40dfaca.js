434e65138ab0e1f186cc0d6582438b5734ccaa6e
Danielkakona <Daniel@bridgelabs.design>
2020-09-09 11:22:12 +0200

    initial setup

---
+++ build/precache-manifest.1a49e8a0e569427fa7fa10b9e40dfaca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24d3cd3cf5b8b4b7489181b0b43c0d0f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f6944a97a4bbe4d04e00",
+    "url": "/static/css/main.1d791036.chunk.css"
+  },
+  {
+    "revision": "f4f234c47dfca8fe5360",
+    "url": "/static/js/2.3ef2d018.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/static/js/2.3ef2d018.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6944a97a4bbe4d04e00",
+    "url": "/static/js/main.fde77877.chunk.js"
+  },
+  {
+    "revision": "f3260a36ed25041221ee",
+    "url": "/static/js/runtime-main.b584fe3d.js"
+  }
+]);
