7c939d5f50175bdeaedde9e9a38ad4bbb457db16
Danielkakona <Daniel@bridgelabs.design>
2020-09-09 11:41:47 +0200

    Buildfor deploy

---
+++ build/precache-manifest.1c6dc7b17652f2a5b0acd4c3900532d5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "deaccd16847a9f0f2c14b5430c1d27b1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7bff6f3216e4737a9598",
+    "url": "/static/css/main.1d791036.chunk.css"
+  },
+  {
+    "revision": "f4f234c47dfca8fe5360",
+    "url": "/static/js/2.3ef2d018.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/static/js/2.3ef2d018.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7bff6f3216e4737a9598",
+    "url": "/static/js/main.0d05f38c.chunk.js"
+  },
+  {
+    "revision": "f3260a36ed25041221ee",
+    "url": "/static/js/runtime-main.b584fe3d.js"
+  }
+]);
